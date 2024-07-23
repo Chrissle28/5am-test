92fa1798b0d7bcbd465ad269832759e1d3c12893
Christian Zellier <69738385+Chrissle28@users.noreply.github.com>
2024-07-23 15:25:08 +0200

    update font weight on dialog title

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -12,12 +12,12 @@
             class="fixed [width:calc(100%-40px)] lg:w-[800px] top-1/2 left-1/2 z-50 p-6 bg-white shadow-[0_0_40px_0_rgba(0,0,0,0.1)] transition duration-300 -translate-x-1/2 -translate-y-1/2 rounded-[24px]"
         >
             <div class="flex justify-between items-center">
-                <div class="font-bold text-[28px] leading-[34px]">
+                <div class="text-[28px] leading-[34px]">
                     {{ dialogTitle }}
                 </div>
                 <ButtonClose />
             </div>
-            <p class="mt-4 font-medium text-black/50 max-w-[600px]">
+            <p class="mt-4 text-black/50 max-w-[600px]">
                 {{ dialogText }}
             </p>
             <div class="my-6 w-full h-px bg-black/20"></div>
